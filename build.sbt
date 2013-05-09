ddce8a5dadae1015961e73d2a283bb38e0ba7858
zhaoyou <zhaoyou.xt@gmail.com>
2013-05-09 13:33:33 +0800

    temp

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ scalaVersion := "2.10.0"
 
 libraryDependencies ++= Seq(
   "net.databinder.dispatch" %% "dispatch-core" % "0.10.0",
-  "net.databinder" % "dispatch-lift-json_2.7.5" % "0.8.0.Beta1"
+  "net.liftweb" % "lift-json_2.10" % "2.5-RC5"
 )
 
 
--- src/main/scala/TradesFetchApp.scala
+++ src/main/scala/TradesFetchApp.scala
@@ -1,14 +1,25 @@
 import dispatch._, Defaults._
-import dispatch.liftjson.Js._
+
 import net.liftweb.json.JsonAST._
 
 object TradesFetchApp {
 
   def run() {
-    println("ok")
-    for (t <- trades(gtmoxUrl(0l))) {
-      println(t)
-    }
+    val http = new Http()
+    val u = url("https://data.mtgox.com/api/1/BTCUSD/trades/fetch?since=0")
+    val x = http(u OK as.String)
+    println(x())
+    //http(u ># { json =>
+    //  (json \ "title" children) flatMap( _ match {
+    //    case JField("title", JString(d)) => Some(d)
+    //    case JString(d) => Some(d)
+    //    case _ => None
+    //  })
+    //})
+    //println("ok")
+    //for (t <- trades(gtmoxUrl(0l))) {
+    //  println(t)
+    //}
   }
 
   // get gtmox trans full, since tid.
