de01f2cdeeae25245ff1f61a857589efb95973e7
zhaoyou <zhaoyou.xt@gmail.com>
2013-05-10 00:26:56 +0800

    add liftjson

--- src/main/scala/TradesFetchApp.scala
+++ src/main/scala/TradesFetchApp.scala
@@ -1,6 +1,7 @@
 import dispatch._, Defaults._
 
-import net.liftweb.json.JsonAST._
+
+import net.liftweb.json._
 
 object TradesFetchApp {
 
@@ -8,7 +9,15 @@ object TradesFetchApp {
     val http = new Http()
     val u = url("https://data.mtgox.com/api/1/BTCUSD/trades/fetch?since=0")
     val x = http(u OK as.String)
-    println(x())
+    for(str <- x) {
+      val jlist = parse(str)
+      println((jlist \ "return")(0))
+      val a =  for { JObject(t) <- (jlist \ "return")
+            JField("tid", JString(tid)) <- t
+          } yield (tid)
+      println(a)
+      //println(jlist)
+    }
     //http(u ># { json =>
     //  (json \ "title" children) flatMap( _ match {
     //    case JField("title", JString(d)) => Some(d)
